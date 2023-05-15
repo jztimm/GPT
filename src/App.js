05f6e114ea67a1433c5f6267f69cc47d45a4368a
jztimm <jztimm@gmail.com>
2023-05-15 08:39:12 -0400

    Completed Possibility Comp and Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { Footer, Blog, Possibility, WhatGPT3, Header } from './containers'
-import { CTA, Brand, Navbar, Feature } from './components'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
 import './App.css'
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
       </div>
       <Brand />
       <WhatGPT3 />
-      <Feature />
+      <Features />
       <Possibility />
       <CTA />
       <Blog />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './navbar.css'
 const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#wgpt3">what is GPT3?</a></p>
+    <p><a href="#whatgpt3">what is GPT3?</a></p>
     <p><a href="#possibility">Open AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
